9f95605763ba8ec47cbc2c52f9e13eced1f11f77
Piatnytskiy Danylo <historypiat@gmail.com>
2026-06-15 13:53:29 +0000

    Update main.c

--- university/C/other/summer practice/task 1/main.c
+++ university/C/other/summer practice/task 1/main.c
@@ -6,38 +6,38 @@ int main() {
     double a, b, c, r, h, v;
     do {
         printf("\nЩо обчислити?\n1. Призма\n2. Циліндр\n3. Куля\n4. Конус\n5. Піраміда\n0. Вихід\nВаш вибір: ");
-        scanf("%d", &choice);
+        scanf("%d",&choice);
 
         switch (choice) {
             case 1:
                 printf("Довжина, ширина, висота: ");
-                scanf("%lf %lf %lf", &a, &b, &c);
-                v = box(a, b, c);
-                printf("Об'єм = %.2f\n", v);
+                scanf("%lf %lf %lf", &a,&b,&c);
+                v = box(a,b,c);
+                printf("Об'єм = %.2f\n",v);
                 break;
             case 2:
                 printf("Радіус, висота: ");
-                scanf("%lf %lf", &r, &h);
-                v = cylinder(r, h);
-                printf("Об'єм = %.2f\n", v);
+                scanf("%lf %lf", &r,&h);
+                v = cylinder(r,h);
+                printf("Об'єм = %.2f\n",v);
                 break;
             case 3:
                 printf("Радіус: ");
-                scanf("%lf", &r);
+                scanf("%lf",&r);
                 v = sphere(r);
-                printf("Об'єм = %.2f\n", v);
+                printf("Об'єм = %.2f\n",v);
                 break;
             case 4:
                 printf("Радіус, висота: ");
-                scanf("%lf %lf", &r, &h);
-                v = cone(r, h);
-                printf("Об'єм = %.2f\n", v);
+                scanf("%lf %lf",&r,&h);
+                v = cone(r,h);
+                printf("Об'єм = %.2f\n",v);
                 break;
             case 5:
                 printf("Сторони основи, висота: ");
-                scanf("%lf %lf %lf", &a, &b, &h);
-                v = pyramid(a, b, h);
-                printf("Об'єм = %.2f\n", v);
+                scanf("%lf %lf %lf",&a,&b,&h);
+                v = pyramid(a,b,h);
+                printf("Об'єм = %.2f\n",v);
                 break;
             case 0:
                 printf("Вихід.\n");
@@ -45,6 +45,6 @@ int main() {
             default:
                 printf("Невірний вибір!\n");
         }
-    } while (choice != 0);
+    } while(choice != 0);
     return 0;
 }
